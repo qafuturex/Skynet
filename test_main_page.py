ca57dd412e8610a47de90b095ec5a3bc1ea25cf0
anvar.sqa <41168694+qafuturex@users.noreply.github.com>
2020-01-20 20:45:43 +0300

    pages folder with files added

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,16 @@
+__author__ = 'anvar.sqa@gmail.com'
 import time
 
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
+    go_to_login_page(browser)
+    time.sleep(5)
+
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
-    time.sleep(5)
