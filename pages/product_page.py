e2eb699b395a1fa946ef184817f26a055f8de33a
anvar.sqa <41168694+qafuturex@users.noreply.github.com>
2020-02-03 21:45:05 +0300

    разделил проверки совпадения цены и имени товара

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,10 +8,15 @@ class ProductPage(BasePage):
         add_to_cart_button.click()
 
     def should_be_message_product_added(self):
+        self.should_be_correct_price_in_message()
+        self.should_be_correct_product_name_in_message()
+
+    def should_be_correct_product_name_in_message(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         product_name_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE).text
+        assert product_name_in_message == product_name, "Wrong product name"
+
+    def should_be_correct_price_in_message(self):
         price = self.browser.find_element(*ProductPageLocators.PRICE).text
         price_in_message = self.browser.find_element(*ProductPageLocators.PRICE_IN_MESSAGE).text
-
-        assert product_name_in_message == product_name, "wrong product name"
         assert price_in_message == price, "Wrong price"
